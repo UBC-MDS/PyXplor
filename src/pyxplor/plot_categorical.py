794c537f08f0e031378b9c2a451ad3d89164e530
rbouwer <45149769+rbouwer@users.noreply.github.com>
2024-01-12 14:35:52 -0800

    Update src/pyxplor/plot_categorical.py to remove orientation parameter

--- src/pyxplor/plot_categorical.py
+++ src/pyxplor/plot_categorical.py
@@ -3,7 +3,6 @@ import pandas as pd
 def plot_categorical(
         input_df: pd.DataFrame,
         list_of_variables: list,
-        orientation: str='v',
         fig_width: int=10,
         fig_height: int=6,
         label_y_offset: int=0,
