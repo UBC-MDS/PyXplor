4c5105eed1b245c0ce017658b40001e17282c77b
iris LUO <nianyiLuo2023@163.com>
2024-01-12 17:07:50 -0800

    update numeric function docstring

---
+++ src/pyxplor/plot_numeric.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import math
+
+
+def plot_numeric(
+        input_df: pd.DataFrame,
+        list_of_variables: list,
+        plot_kind: str,
+        label_y_offset: int,
+        label_fontsize: int,
+        figsize: tuple = (15, 20),
+        output_path: str = "numeric_distribution.png",
+        super_title: str = "Distribution of Numeric Variables",
+        super_title_font: int = 14) -> None:
+    """Plot the distribution of numeric variables in a DataFrame, save the plot, and display it.
+
+    This function will construct a set of subplots for each of the numeric variables specified
+    in `list_of_variables`. The function will save as well as display the entire figure.
+
+    Parameters
+    ----------
+    input_df : pd.DataFrame
+        The DataFrame that contains the numeric variables.
+
+    list_of_variables : list
+        List of numeric variables (column names) to be plotted.
+
+    figsize: tuple[int, int]:
+        The width and height of the figure size in a tuple.
+
+    plot_kind : {'hist', 'kde', 'hist+kde'}
+        Type of plot to be generated:
+        'hist': Histogram only.
+        'kde': Kernel density estimate plot only.
+        'hist+kde': Histogram with a kernel density estimate plot showing median and mean.
+
+    label_y_offset : int
+        Y-axis offset for label positioning.
+
+    label_fontsize : int
+        Font size for axis labels.
+
+    output_path : str, optional
+        Path to save the plot. Defaults to "numeric_distribution.png".
+
+    super_title : str, optional
+        Super title for the entire plot. Default is "Distribution of Numeric Variables".
+
+    super_title_font : int, optional
+        Font size for the super title. Default is 14.
+
+    Returns
+    -------
+    None
+
+
+    Examples
+    --------
+    numeric = ["numeric_var1", "numeric_var2"]
+    plot_numeric(df, numeric, "hist+kde", 30, 10, (15, 20), "numeric_distribution.png", "Distribution of Numeric Variables", 14)
+    """
+
+    pass # Implementation to be added
