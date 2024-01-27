66c38f7920d653bcc0d8cf1bce708fb8468d9ca8
Po-Hsun Chen <benjamin990126@hotmail.com>
2024-01-27 15:41:35 -0800

    add logo

--- docs/conf.py
+++ docs/conf.py
@@ -33,4 +33,11 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_t
+
+# Adding logo
+html_static_path = ['_static']
+html_logo = 'img/pyxplor_logo.png'
+html_theme_options = {
+    'logo_only': True
+}
